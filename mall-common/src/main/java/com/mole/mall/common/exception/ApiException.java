5572b3479429e85eb7e556fb669e594fde1c5a71
Cyanix-0721 <34270450+Cyanix-0721@users.noreply.github.com>
2024-09-28 23:48:10 +0800

    feat: 自定义API异常

--- mall-common/src/main/java/com/mole/mall/common/exception/ApiException.java
+++ mall-common/src/main/java/com/mole/mall/common/exception/ApiException.java
@@ -1,32 +1,31 @@
 package com.mole.mall.common.exception;
 
-import com.macro.mall.common.api.IErrorCode;
+import com.mole.mall.common.api.IErrorCode;
+import lombok.Getter;
 
 /**
  * 自定义API异常
  * Created by macro on 2020/2/27.
  */
+@Getter
 public class ApiException extends RuntimeException {
-    private IErrorCode errorCode;
+	private IErrorCode errorCode;
 
-    public ApiException(IErrorCode errorCode) {
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
-    }
+	public ApiException(IErrorCode errorCode) {
+		super(errorCode.getMessage());
+		this.errorCode = errorCode;
+	}
 
-    public ApiException(String message) {
-        super(message);
-    }
+	public ApiException(String message) {
+		super(message);
+	}
 
-    public ApiException(Throwable cause) {
-        super(cause);
-    }
+	public ApiException(Throwable cause) {
+		super(cause);
+	}
 
-    public ApiException(String message, Throwable cause) {
-        super(message, cause);
-    }
+	public ApiException(String message, Throwable cause) {
+		super(message, cause);
+	}
 
-    public IErrorCode getErrorCode() {
-        return errorCode;
-    }
 }
