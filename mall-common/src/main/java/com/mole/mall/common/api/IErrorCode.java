ec3fe62a9e18d64bef73eaecf16228762b954ede
Cyanix-0721 <34270450+Cyanix-0721@users.noreply.github.com>
2024-09-28 23:46:58 +0800

    feat: 通用返回

--- mall-common/src/main/java/com/mole/mall/common/api/IErrorCode.java
+++ mall-common/src/main/java/com/mole/mall/common/api/IErrorCode.java
@@ -5,7 +5,7 @@ package com.mole.mall.common.api;
  * Created by macro on 2019/4/19.
  */
 public interface IErrorCode {
-    long getCode();
+	long getCode();
 
-    String getMessage();
+	String getMessage();
 }
--- mall-common/src/main/java/com/mole/mall/common/api/ResultCode.java
+++ mall-common/src/main/java/com/mole/mall/common/api/ResultCode.java
@@ -1,28 +1,24 @@
 package com.mole.mall.common.api;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 /**
  * 枚举了一些常用API操作码
  * Created by macro on 2019/4/19.
+ * Modify by Cyanix on 2024/9/27.
  */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public enum ResultCode implements IErrorCode {
-    SUCCESS(200, "操作成功"),
-    FAILED(500, "操作失败"),
-    VALIDATE_FAILED(404, "参数检验失败"),
-    UNAUTHORIZED(401, "暂未登录或token已经过期"),
-    FORBIDDEN(403, "没有相关权限");
-    private long code;
-    private String message;
+	SUCCESS(200, "操作成功"),
+	FAILED(500, "操作失败"),
+	VALIDATE_FAILED(404, "参数检验失败"),
+	UNAUTHORIZED(401, "暂未登录或token已经过期"),
+	FORBIDDEN(403, "没有相关权限");
 
-    private ResultCode(long code, String message) {
-        this.code = code;
-        this.message = message;
-    }
-
-    public long getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+	private long code;
+	private String message;
 }
